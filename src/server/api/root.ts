51282dc313c00a5111b53448d6c6c627b3063bc1
SHAHID-ab57 <shahidakrambiswas@gmail.com>
2025-02-12 23:24:53 +0530

    update

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+// import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { todoRouter } from "./routers/Todo";
 import { userRouter } from "./routers/user";
@@ -9,7 +9,7 @@ import { userRouter } from "./routers/user";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  // post: postRouter,
   todo: todoRouter,
   user:userRouter
 });
