ea4a2506ea3b45adda6a7afa306dc2732cf96878
SHAHID-ab57 <shahidakrambiswas@gmail.com>
2025-02-12 23:50:18 +0530

    update in bcrypt

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import bcrypt from "bcryptjs";
+const bcrypt = require("bcryptjs");
+
 
 export const userRouter = createTRPCRouter({
   // Create a new user (password is hashed before saving)
